ec456d57766e22d3224c8e75594bf564c5d29cf3
Okek <87618530+skuyykek69@users.noreply.github.com>
2025-01-28 13:45:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ cekIpButton.addEventListener('click', () => {
     fetch('https://ipapi.co/json/')
         .then(response => response.json())
         .then(data => {
+            // Menampilkan informasi dasar
             hasilDiv.innerHTML = `
                 <p>IP Address: ${data.ip}</p>
                 <p>City: ${data.city}</p>
                 <p>Region: ${data.region}</p>
                 <p>Country: ${data.country}</p>
                 <p>Timezone: ${data.timezone}</p>
-                <p>Koordinat: ${data.latlong}</p>
-                <p>ISP: ${data.isp}</p>
-                <p>Kode Pos: ${data.postal}</p>
-                <p>Mata Uang: ${data.currency}</p>
-                `;
+            `;
+
+            // Menampilkan waktu real-time berdasarkan zona waktu
+            const waktuSekarang = dayjs().tz(data.timezone);
+            hasilDiv.innerHTML += `<p>Waktu Lokal: ${waktuSekarang.format('HH:mm:ss DD MMMM YYYY')}</p>`;
         })
         .catch(error => {
             console.error('Error:', error);
