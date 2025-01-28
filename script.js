4cf1df450d4c3c30729a4e448a4d9f82be67130a
Okek <87618530+skuyykek69@users.noreply.github.com>
2025-01-28 14:01:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const cekIpButton = document.getElementById('cek-ip');
 const hasilDiv = document.getElementById('hasil');
 
@@ -5,19 +12,16 @@ cekIpButton.addEventListener('click', () => {
     fetch('https://ipapi.co/json/')
         .then(response => response.json())
         .then(data => {
-            // Menampilkan informasi dasar
+            const waktuSekarang = dayjs().tz(data.timezone);
             hasilDiv.innerHTML = `
                 <p>IP Address: ${data.ip}</p>
                 <p>City: ${data.city}</p>
                 <p>Region: ${data.region}</p>
                 <p>Country: ${data.country}</p>
                 <p>Timezone: ${data.timezone}</p>
-                <p>Koordinat: ${data.latitude}, ${data.longtitude}</p>
+                <p>Koodinat: ${data.latitude}, ${data.longtitude}</p>
+                <p>Waktu Lokal: ${waktuSekarang.format('HH:mm:ss DD MMMM YYYY')}</p>
             `;
-
-            // Menampilkan waktu real-time berdasarkan zona waktu
-            const waktuSekarang = dayjs().tz(data.timezone);
-            hasilDiv.innerHTML += `<p>Waktu Lokal: ${waktuSekarang.format('HH:mm:ss DD MMMM YYYY')}</p>`;
         })
         .catch(error => {
             console.error('Error:', error);
